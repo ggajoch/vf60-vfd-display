5d28b8c61830d9137b2594de49a8aa6e1b6a1dbc
Grzegorz Gajoch <gajoch@gmail.com>
2019-02-05 16:44:27 +0100

    Justify text

--- clock.py
+++ clock.py
@@ -1,14 +1,15 @@
 import time
 from vf60.VFD import VFD
 
-vfd = VFD('COM26', rtscts=True)
+vfd = VFD('/dev/ttyUSB0', rtscts=True)
 
 vfd.clear_display()
 
 last_timestr = ''
 while True:
     time.sleep(0.1)
-    timestr = time.strftime("      %H:%M:%S", time.localtime(time.time()))
+    now = time.localtime(time.time())
+    timestr = time.strftime("%H:%M:%S", now).center(20)
     if timestr == last_timestr:
         continue
 
@@ -16,7 +17,7 @@ while True:
     vfd.set_cursor(1, 1)
     vfd.write(timestr)
 
-    date = time.strftime("%A    %d.%m.%Y", time.localtime(time.time()))
+    date = time.strftime("%A", now).ljust(10) + time.strftime("%d.%m.%Y", now).rjust(10)
 
     vfd.set_cursor(1, 2)
     vfd.write(date)
